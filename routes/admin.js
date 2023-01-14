ec0dfa8e08315ba49439f0337e4c697a970c153d
santhoshv <santhosh.v1@flipkart.com>
2023-01-14 20:12:03 +0530

    added pug and ejs files

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,22 @@
-const express = require("express")
-const router = express.Router()
-const path = require("path");
-const rootDir = require("../util/path")
+const path = require('path');
 
-console.log(rootDir)
-const product =[]
-router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(rootDir,"views","add-product.html"))
-})
+const express = require('express');
 
-router.post("/add-product", (req, res, next) => {
-    console.log(req.body)
-    product.push({title:req.body.title})
-    res.redirect("/")
-})
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const product = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  product.push({ title: req.body.title });
+  res.redirect('/');
+});
 
 module.exports = {router,product};
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,11 @@ const express = require("express")
 const router = express.Router();
 const path = require("path")
 
-const rootDir = require("../util/path")
+const rootDir = require("../util/path");
+const { product } = require("./admin");
 
 router.get("/", (req, res, next) => {
-    res.render("shop")
+    res.render("shop",{prods:product})
 })
 
 module.exports = router
