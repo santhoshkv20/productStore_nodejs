56480c2de2482366a0d36dfe4c8dcc13f76a3af7
santhoshv <santhosh.v1@flipkart.com>
2023-01-15 11:29:04 +0530

    finish controller

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path")
 const bodyParser = require("body-parser");
 const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
+const { getError } = require("./controllers/error");
 
 
 const app = express();
@@ -15,7 +16,5 @@ app.use("/admin",adminRouter.router)
 app.use(shopRouter)
 app.use(express.static(path.join(__dirname,"public")))
 
-app.use((req,res,next)=>{
-res.render("404")
-})
+app.use("/",getError)
 app.listen(3000);
