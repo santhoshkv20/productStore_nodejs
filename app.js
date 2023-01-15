8201d413b5fec9aef85ad44bc259d8abe65ca691
santhoshv <santhosh.v1@flipkart.com>
2023-01-15 11:09:55 +0530

    code clean up

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const shopRouter = require("./routes/shop");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}))
-app.set("view engine","pug")
+app.set("view engine","ejs")
 app.set("views","views")
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,14 @@ const rootDir = require("../util/path");
 const { product } = require("./admin");
 
 router.get("/", (req, res, next) => {
-    res.render("shop",{prods:product})
+    res.render('shop', {
+        prods: product,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: product.length > 0,
+        activeShop: true,
+        productCSS: true
+      });
 })
 
 module.exports = router
