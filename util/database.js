d1468b75777224cd8a1ba45d429eae25c7f1c4ea
santhoshv <santhosh.v1@flipkart.com>
2023-01-24 09:56:54 +0530

    mongidb connection

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb")
+const mongodbClient = mongodb.MongoClient
+const mongoClient = (callback)=>{
+  mongodbClient.connect("mongodb+srv://skv201:m4xgrRaIgzdmahea@cluster0.5yjzbfs.mongodb.net/?retryWrites=true&w=majority")
+  .then(res=>{
+  console.log('connected')
+  callback(res)
+  }).catch(err=>{
+    console.log(err)
+  })
+}
+module.exports = mongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
 
-module.exports = sequelize;
