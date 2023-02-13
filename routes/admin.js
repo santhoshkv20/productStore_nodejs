a12729baad2e38163cf35025e96ade3aa4d09414
Santhosh V <santhosh.v1@flipkart.com>
2023-02-13 14:06:05 +0530

    added validation for add product

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const {check, body} = require("express-validator")
 const is_auth = require("../middleware/is-auth");
 
 const router = express.Router();
@@ -14,7 +15,14 @@ router.get('/add-product',is_auth, adminController.getAddProduct);
 router.get('/products', is_auth,adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product',is_auth, adminController.postAddProduct);
+router.post('/add-product', [
+    body("title").isString().isLength({ min: 4 }).trim().withMessage("Check title"),
+    body("imageUrl").isURL().trim().withMessage("Check URL"),
+    body("price").isFloat().withMessage("Price should have decimal"),
+    body("description").isAlphanumeric().isLength({ min: 10 }).trim().withMessage("Description must be long")
+
+
+], is_auth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId',is_auth, adminController.getEditProduct);
 
